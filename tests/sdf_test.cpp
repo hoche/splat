75d180b2ea7c68f29e310418d042c4c9a5d2897e
Michel Hoche-Mong <hoche31@gmail.com>
2025-10-26 17:38:30 -0700

    Check return value of system() calls in tests.

--- tests/sdf_test.cpp
+++ tests/sdf_test.cpp
@@ -24,7 +24,8 @@ class SdfTest : public ::testing::Test {
         em = new ElevationMap(*sr);
 
         // Create test directory
-        system("mkdir -p ./test_sdf_data");
+        int ret = system("mkdir -p ./test_sdf_data");
+        (void)ret;  // Ignore return value - test will fail later if dir not created
     }
 
     void TearDown() override {
@@ -32,7 +33,8 @@ class SdfTest : public ::testing::Test {
         delete sr;
 
         // Clean up test files
-        system("rm -rf ./test_sdf_data");
+        int ret = system("rm -rf ./test_sdf_data");
+        (void)ret;  // Ignore return value - cleanup failure is not critical
     }
 
     // Helper to create a minimal valid SDF file
@@ -254,13 +256,15 @@ class SdfCoordinateTest
         sr->sdf_path = "./test_sdf_coords";
         sr->ppd = 1200;
         em = new ElevationMap(*sr);
-        system("mkdir -p ./test_sdf_coords");
+        int ret = system("mkdir -p ./test_sdf_coords");
+        (void)ret;  // Ignore return value - test will fail later if dir not created
     }
 
     void TearDown() override {
         delete em;
         delete sr;
-        system("rm -rf ./test_sdf_coords");
+        int ret = system("rm -rf ./test_sdf_coords");
+        (void)ret;  // Ignore return value - cleanup failure is not critical
     }
 };
 
